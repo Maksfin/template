386f43a89105d974a2d2ea2759948656fa675436
Maksfin <dyrnoj@bk.ru>
2015-12-18 01:51:28 +0300

    created my template

---
+++ README.md
@@ -0,0 +1,12 @@
+КАК УСТАНОВИТЬ МОЙ ПРОЕКТ
+
+1. git clone https://github.com/Maksfin/template.git site
+
+2. cd site
+
+3. bower i
+
+4. npm i
+
+5. gulp
+
---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function($) {
+
+	$('.form').on('click', function(e) {
+		e.preventDefault();
+
+		
+	});
+
+}); // --> end
+
+$.fn.tooltip = function(options) {
+
+	var options = {
+		position: options.position || 'right',
+		
+	}
+
+}
